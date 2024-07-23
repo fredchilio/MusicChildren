e100ee66f14d33c3e4bf16601d0d1dc47c4c6058
Frederico Chilio <fredchilio@yahoo.com.br>
2024-07-24 00:34:59 +0100

    Removido os ID

--- assets/JS/notas.js
+++ assets/JS/notas.js
@@ -1,110 +1,65 @@
- 
-var musica1 = [
-    ['Tinkle1'],
-    ['do', 'do', 'sol','sol','la','la','sol','fa','fa','mi','mi','re','re','do'],
-    ['sol','sol','fa','fa','mi','mi','re','sol','sol','fa','fa','mi','mi','re'],
-    ['do', 'do', 'sol','sol','la','la','sol','fa','fa','mi','mi','re','re','do']];
-    var musica2 = [
-    ['Tinkle2'],
-    ['re', 're', 'la','sol','la','la','sol','fa','fa','mi','mi','re','re','do'],
-    ['sol','sol','fa','fa','mi','mi','re','sol','sol','fa','fa','mi','mi','re'],
-    ['do', 'do', 'sol','sol','la','la','sol','fa','fa','mi','mi','re','re','do']];
-    var musica3 = [
-    ['Tinkle3'],
-    ['mi', 'mi', 'si','sol','la','la','sol','fa','fa','mi','mi','re','re','do'],
-    ['sol','sol','fa','fa','mi','mi','re','sol','sol','fa','fa','mi','mi','re'],
-    ['do', 'do', 'sol','sol','la','la','sol','fa','fa','mi','mi','re','re','do']];
+// Definição das músicas
+const musicas = {
+    musica1: [
+        ['Tinkle1'],
+        ['do', 'do', 'sol', 'sol', 'la', 'la', 'sol', 'fa', 'fa', 'mi', 'mi', 're', 're', 'do'],
+        ['sol', 'sol', 'fa', 'fa', 'mi', 'mi', 're', 'sol', 'sol', 'fa', 'fa', 'mi', 'mi', 're'],
+        ['do', 'do', 'sol', 'sol', 'la', 'la', 'sol', 'fa', 'fa', 'mi', 'mi', 're', 're', 'do']
+    ],
+    musica2: [
+        ['Do Re Mi Fa'],
+        ['do', 're', 'mi', 'fa', 'fa', 'fa', 'do', 're', 'do', 're', 're', 're', 'do', 'sol'],
+        ['fa', 'mi', 'mi', 'mi', 'do', 're', 'mi', 'fa', 'fa', 'fa', '', '', '', ''],
+        ['do', 'fa', 'mi', 're', 'do', 're', 'do', 'do', 'do', 're', 'do', 're', 'mi', 'fa']
+    ],
+    musica3: [
+        ['Black Shep'],
+        ['do', 'do', 'sol', 'sol', 'la', 'la', 'sol', 'fa', 'fa', 'mi', 'mi', 're', 're', 'do'],
+        ['sol', 'sol', 'fa', 'fa', 'mi', 'mi', 're', 'sol', 'sol', 'fa', 'fa', 'mi', 'mi', 're'],
+        ['do', 'do', 'sol', 'sol', 'la', 'la', 'sol', 'fa', 'fa', 'mi', 'mi', 're', 're', 'do']
+    ]
+};
 
-var notes = [
-   ['Tinkle'],
-   ['do', 'do', 'sol','sol','la','la','sol','fa','fa','mi','mi','re','re','do'],
-   ['sol','sol','fa','fa','mi','mi','re','sol','sol','fa','fa','mi','mi','re'],
-   ['do', 'do', 'sol','sol','la','la','sol','fa','fa','mi','mi','re','re','do']];
-    
-   //var cssStart = [notes]    
-        
-      
-             
-              //function sheetMusic(notas){
-              
-                var notes_numbers=0;
-                
-                //for (var a = 0; a<cssStart.length; a++){//dois dias para conseguir colocar as figuras do css no estado inicial (none)
-                    //cssStart[a].style.display='none'
-               // }
-                cssStart=[]
-                     
+// Mapeamento das notas para números
+const notaParaNumero = {
+    'do': 12,
+    're': 11,
+    'mi': 10,
+    'fa': 9,
+    'sol': 8,
+    'la': 7,
+    'si': 6,
+    'do+': 5,
+    're+': 4,
+    'mi+': 3,
+    'fa+': 2,
+    'sol+': 1
+};
+
+// Função para exibir a partitura
+function exibirPartitura(musica) {
+    // Definir o título da partitura
+    document.getElementById('tittle').innerHTML = musica[0][0];
+
+    // Iterar pelas linhas da partitura
+    for (let linha = 1; linha < musica.length; linha++) {
+        for (let posicao = 0; posicao < musica[linha].length; posicao++) {
+            const nota = musica[linha][posicao];
+            const numeroNota = notaParaNumero[nota] || 0;
+
+            // Definir a variável para a linha e a posição
+            const x = posicao + 2; // Ajustar a posição
+            const y = linha;
+
+            // Selecionar o elemento correspondente e exibir a imagem
+            const elemento = document.querySelector(`body .pentagrama_${y} .linhas_espacos:nth-child(${x}) div:nth-child(${numeroNota}) img`);
             
-               
-                
-               
-               
-                
-
-cssStart = musica3
-//
-
-document.getElementById('tittle').innerHTML=cssStart[0][0]
-for(var z = 1;z<4;z++ ){
-    for (var i = 0; i<cssStart[z].length; i++){ // demorei muito para descobrir o tamanho(lenght) de um arrary bidimensional
-        
-        switch(cssStart[z][i]){
-            case "do":
-                notes_numbers=12
-                break;
-            case "re":
-                notes_numbers=11
-                break;           
-            case "mi":
-                notes_numbers=10
-                break;
-            case "fa":
-                notes_numbers=9
-                break;
-            case "sol":
-                notes_numbers=8
-                break;
-            case "la":
-                notes_numbers=7
-                break;
-            case "si":
-                notes_numbers=6
-                break;
-            case "do+":
-                notes_numbers=5
-                break;
-            case "re+":
-                notes_numbers=4
-                break;
-            case "mi+":
-                notes_numbers=3
-                break;
-            case "fa+":
-                notes_numbers=2
-                break;
-            case "sol+":
-                notes_numbers=1
-                break;
-            case"":
-                break;
-                
+            if (elemento) {
+                elemento.style.display = 'block'; // Mostrar a imagem
+            }
         }
-        
-    
-    var x = i+2;//para nao afetar o 'i' tive que criar uma variavel x para aplicar no comando abaixo
-    var y = z;
-    
-    (document.querySelector(`body .pentagrama_${y} .linhas_espacos:nth-child(${x}) \
-    div:nth-child(${notes_numbers}) img`)).style.display='block';//essa barra '\' eh utilizada para separar o codigo em mais de uma linha
-    //cssStart[i].style.display='block.';
-    
-    notes_numbers=0;
-    
     }
-    
-
-//document.querySelector(`body .pentagrama_${y} .linhas_espacos:nth-child(${x}) h\
-//div:nth-child(${12}) img`).style.backgroundImage = "url('./img/loading-6')";
-
-
 }
+
+// Chamar a função com a música desejada
+exibirPartitura(musicas.musica3);
