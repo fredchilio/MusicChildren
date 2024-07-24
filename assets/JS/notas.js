4f2f76dbb0b5554926a480d29f778345a64d5fd4
Frederico Chilio <fredchilio@yahoo.com.br>
2024-07-24 04:06:51 +0100

    Os links estao funcionando, porem precisa arrumar alguns bugs

--- assets/JS/notas.js
+++ assets/JS/notas.js
@@ -1,4 +1,4 @@
-// Definição das músicas
+//Definição das músicas
 const musicas = {
     musica1: [
         ['Tinkle1'],
@@ -62,4 +62,52 @@ function exibirPartitura(musica) {
 }
 
 // Chamar a função com a música desejada
-exibirPartitura(musicas.musica3);
+exibirPartitura(musicas.musica2);
+/***const notaParaNumero = {
+    'do': 12,
+    're': 11,
+    'mi': 10,
+    'fa': 9,
+    'sol': 8,
+    'la': 7,
+    'si': 6,
+    'do+': 5,
+    're+': 4,
+    'mi+': 3,
+    'fa+': 2,
+    'sol+': 1
+};
+
+function exibirPartitura(musica) {
+    // Limpar partitura existente
+    for (let i = 1; i <= 3; i++) {
+        const pentagrama = document.querySelector(`#pentagrama${i}`);
+        pentagrama.innerHTML = '';
+        for (let j = 0; j < 5; j++) {
+            pentagrama.innerHTML += `<div class="linha" id="linha${(i - 1) * 5 + j + 1}"></div>`;
+            pentagrama.innerHTML += `<div class="espaco" id="espaco${(i - 1) * 5 + j + 1}"></div>`;
+        }
+    }
+
+    // Definir o título da partitura
+    document.getElementById('tittle').innerHTML = musica[0][0];
+
+    // Iterar pelas linhas da partitura
+    for (let linha = 1; linha < musica.length; linha++) {
+        for (let posicao = 0; posicao < musica[linha].length; posicao++) {
+            const nota = musica[linha][posicao];
+            const numeroNota = notaParaNumero[nota] || 0;
+
+            // Criar um elemento para a nota
+            const noteElement = document.createElement('div');
+            noteElement.classList.add('note');
+            noteElement.style.gridRow = numeroNota;
+
+            // Adicionar a nota ao pentagrama
+            const pentagrama = document.querySelector(`#pentagrama${linha}`);
+            if (pentagrama) {
+                pentagrama.appendChild(noteElement);
+            }
+        }
+    }
+}***/
